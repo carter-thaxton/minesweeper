e601e5c72961d4d04d7ece2e88eebb8710a2b505
Carter Thaxton <carter.thaxton@gmail.com>
2023-02-19 15:01:32 -0800

    Initial commit, using egui and eframe

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+#![warn(clippy::all, rust_2018_idioms)]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
+mod app;
+
+fn main() -> eframe::Result<()> {
+    // Log to stdout (if you run with `RUST_LOG=debug`).
+    //tracing_subscriber::fmt::init();
+
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Minesweeper",
+        native_options,
+        Box::new(|cc| Box::new(app::MinesweeperApp::new(cc))),
+    )
+}
